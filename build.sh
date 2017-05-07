2eabc07677f56cf0593ea41ff1c493d7e2696888
Tom <tomcm14@gmail.com>
2017-05-07 15:23:29 -0700

    Created build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Run the build
+
+echo "Creating tables..."
+python dbinit/create_tables.py
+
+echo "Source to stage 1 mappings..."
+python mappings/src_stg1_boxscore.py
+
+echo "Stage 1 to stage 2 mappings..."
+python mappings/stg1_stg2_game_result.py
+
+echo "Stage 2 to stage 2 mappings..."
+python mappings/stg2_stg2_season.py
