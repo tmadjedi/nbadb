a1bacc63d0035b1561d75a03803901e9e0022b87
Tom <tomcm14@gmail.com>
2017-05-20 12:44:02 -0700

    Created team boxscore mapping

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ python mappings/src_stg1_boxscore.py
 
 echo "Stage 1 to stage 2 mappings..."
 python mappings/stg1_stg2_game_result.py
+python mappings/stg1_stg2_team_boxscore.py
 
 echo "Stage 2 to stage 2 mappings..."
 python mappings/stg2_stg2_season_result.py
